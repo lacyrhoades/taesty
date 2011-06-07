096d20050aa305039c3edf712fa65c1d88b21c43
Lacy Rhoades <lacyrhoades@gmail.com>
2011-06-07 16:38:05 -0500

    [app] added basic bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('lib/etsy/Etsy.php');
+require_once('lib/etsy/User.php');
+require_once('lib/etsy/FavoriteUser.php');
+require_once('lib/etsy/ShopListingsActive.php');
+require_once('lib/etsy/Listing.php');
+require_once('lib/etsy/ListingSuggestionService.php');
+
